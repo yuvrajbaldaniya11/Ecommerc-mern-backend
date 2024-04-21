77c6c9e122d667b235b13396a22c177728c69109
yuviahir6677 <ahiryuvraj888@gmail.com>
2024-04-21 12:33:01 +0530

    Done

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -59,269 +59,270 @@ const paymentVerification = async (req, res) => {
   try {
     if (isAuthentic) {
       // Database comes here
-      const transport = nodemailer.createTransport({
-        service: "gmail",
-        host: "smtp.gmail.email",
-        port: 465,
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.EMAIL_PASSWORD
-        },
-      })
-      const mailOptions = {
-        from: process.env.EMAIL,
-        to: userData.userEmail,
-        subject: "Order Confirm",
-        html: `<!DOCTYPE html>
-        <html>
-          <head>
-            <meta charset="UTF-8">
-            <title>Order Confirmation</title>
-            <style>
-              body {
-                font-family: Arial, sans-serif;
-                font-size: 16px;
-                line-height: 1.5;
-                color: black;
-              }
+      // const transport = nodemailer.createTransport({
+      //   service: "gmail",
+      //   host: "smtp.gmail.email",
+      //   port: 465,
+      //   auth: {
+      //     user: process.env.EMAIL,
+      //     pass: process.env.EMAIL_PASSWORD
+      //   },
+      // })
+  //     const mailOptions = {
+  //       from: process.env.EMAIL,
+  //       to: userData.userEmail,
+  //       subject: "Order Confirm",
+  //       html: `<!DOCTYPE html>
+  //       <html>
+  //         <head>
+  //           <meta charset="UTF-8">
+  //           <title>Order Confirmation</title>
+  //           <style>
+  //             body {
+  //               font-family: Arial, sans-serif;
+  //               font-size: 16px;
+  //               line-height: 1.5;
+  //               color: black;
+  //             }
         
-              h1 {
-                font-size: 24px;
-                margin-bottom: 20px;
-                color: black;
-              }
+  //             h1 {
+  //               font-size: 24px;
+  //               margin-bottom: 20px;
+  //               color: black;
+  //             }
         
-              table {
-                width: 100%;
-                border-collapse: collapse;
-                margin-bottom: 20px;
-              }
+  //             table {
+  //               width: 100%;
+  //               border-collapse: collapse;
+  //               margin-bottom: 20px;
+  //             }
         
-              th {
-                text-align: left;
-                padding: 10px;
-                background-color: #eee;
-              }
+  //             th {
+  //               text-align: left;
+  //               padding: 10px;
+  //               background-color: #eee;
+  //             }
         
-              td {
-                padding: 10px;
-                border: 1px solid #ddd;
-              }
+  //             td {
+  //               padding: 10px;
+  //               border: 1px solid #ddd;
+  //             }
         
-              .address {
-                margin-bottom: 20px;
-                color: black;
+  //             .address {
+  //               margin-bottom: 20px;
+  //               color: black;
 
-              }
+  //             }
         
-              .address h2 {
-                font-size: 20px;
-                margin-bottom: 10px;
-              }
+  //             .address h2 {
+  //               font-size: 20px;
+  //               margin-bottom: 10px;
+  //             }
         
-              .address p {
-                margin: 0;
-              }
+  //             .address p {
+  //               margin: 0;
+  //             }
         
-              .thanks {
-                font-size: 18px;
-                margin-top: 20px;
-                color: black;
+  //             .thanks {
+  //               font-size: 18px;
+  //               margin-top: 20px;
+  //               color: black;
 
-              }
+  //             }
         
-              .signature {
-                margin-top: 40px;
-                color: black;
+  //             .signature {
+  //               margin-top: 40px;
+  //               color: black;
 
-              }
+  //             }
         
-              .signature p {
-                margin: 0;
-              }
-            </style>
-          </head>
-          <body>
-            <h1>Order Confirmation</h1>
-            <p style="color:black;">Dear <b>${userData.firstName} ${userData.lastName}</b>,</p>
-            <p>Thank you for your recent purchase on our website. We have received your payment of <b>₹${totalAmount}</b> and have processed your order.</p>
-            <table>
-              <thead>
-                <tr>
-                  <th>Product Name</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                </tr>
-              </thead>
-              <tbody>
-                ${productInfo.map((product) => {
-          return `
-                            <tr>
-                              <td>${product.productId.name}</td>
-                              <td>${product.quantity}</td>
-                              <td>₹${product.productId.price}</td>
-                            </tr>
-                          `
-        }).join('')
-          }
-          <tr>
-          <td>Shipping Charge</td>
-          <td></td>
-          <td>₹100</td>
-        </tr>
-        <tr>
-          <td>Total</td>
-          <td></td>
-          <td>₹${totalAmount}</td>
-        </tr>
-              </tbody >
-            </table >
-            <div class="address">
-              <h2>Shipping Address</h2>
-              <p>${userData.firstName} ${userData.lastName}</p>
-              <p>${userData.address}</p>
-              <p>${userData.city}-${userData.zipCode}</p>
-              <p>${userData.userState}</p>
-            </div>
-            <p class="thanks">Thank you for choosing our website. If you have any questions or concerns, please don't hesitate to contact us.</p>
-            <div class="signature">
-              <p>Best regards,</p>
-              <p> <a href="https://e-shopit.vercel.app/" target="_blank">ShopIt.com</a></p>
-            </div>
-          </body >
-        </html >
-  `,
-        text: `<!DOCTYPE html>
-        <html>
-          <head>
-            <meta charset="UTF-8">
-            <title>Order Confirmation</title>
-            <style>
-              body {
-                font-family: Arial, sans-serif;
-                font-size: 16px;
-                line-height: 1.5;
-                color: black;
-              }
+  //             .signature p {
+  //               margin: 0;
+  //             }
+  //           </style>
+  //         </head>
+  //         <body>
+  //           <h1>Order Confirmation</h1>
+  //           <p style="color:black;">Dear <b>${userData.firstName} ${userData.lastName}</b>,</p>
+  //           <p>Thank you for your recent purchase on our website. We have received your payment of <b>₹${totalAmount}</b> and have processed your order.</p>
+  //           <table>
+  //             <thead>
+  //               <tr>
+  //                 <th>Product Name</th>
+  //                 <th>Quantity</th>
+  //                 <th>Price</th>
+  //               </tr>
+  //             </thead>
+  //             <tbody>
+  //               ${productInfo.map((product) => {
+  //         return `
+  //                           <tr>
+  //                             <td>${product.productId.name}</td>
+  //                             <td>${product.quantity}</td>
+  //                             <td>₹${product.productId.price}</td>
+  //                           </tr>
+  //                         `
+  //       }).join('')
+  //         }
+  //         <tr>
+  //         <td>Shipping Charge</td>
+  //         <td></td>
+  //         <td>₹100</td>
+  //       </tr>
+  //       <tr>
+  //         <td>Total</td>
+  //         <td></td>
+  //         <td>₹${totalAmount}</td>
+  //       </tr>
+  //             </tbody >
+  //           </table >
+  //           <div class="address">
+  //             <h2>Shipping Address</h2>
+  //             <p>${userData.firstName} ${userData.lastName}</p>
+  //             <p>${userData.address}</p>
+  //             <p>${userData.city}-${userData.zipCode}</p>
+  //             <p>${userData.userState}</p>
+  //           </div>
+  //           <p class="thanks">Thank you for choosing our website. If you have any questions or concerns, please don't hesitate to contact us.</p>
+  //           <div class="signature">
+  //             <p>Best regards,</p>
+  //             <p> <a href="https://e-shopit.vercel.app/" target="_blank">ShopIt.com</a></p>
+  //           </div>
+  //         </body >
+  //       </html >
+  // `,
+  //       text: `<!DOCTYPE html>
+  //       <html>
+  //         <head>
+  //           <meta charset="UTF-8">
+  //           <title>Order Confirmation</title>
+  //           <style>
+  //             body {
+  //               font-family: Arial, sans-serif;
+  //               font-size: 16px;
+  //               line-height: 1.5;
+  //               color: black;
+  //             }
         
-              h1 {
-                font-size: 24px;
-                margin-bottom: 20px;
-                color: black;
-              }
+  //             h1 {
+  //               font-size: 24px;
+  //               margin-bottom: 20px;
+  //               color: black;
+  //             }
         
-              table {
-                width: 100%;
-                border-collapse: collapse;
-                margin-bottom: 20px;
-              }
+  //             table {
+  //               width: 100%;
+  //               border-collapse: collapse;
+  //               margin-bottom: 20px;
+  //             }
         
-              th {
-                text-align: left;
-                padding: 10px;
-                background-color: #eee;
-              }
+  //             th {
+  //               text-align: left;
+  //               padding: 10px;
+  //               background-color: #eee;
+  //             }
         
-              td {
-                padding: 10px;
-                border: 1px solid #ddd;
-              }
+  //             td {
+  //               padding: 10px;
+  //               border: 1px solid #ddd;
+  //             }
         
-              .address {
-                margin-bottom: 20px;
-                color: black;
+  //             .address {
+  //               margin-bottom: 20px;
+  //               color: black;
 
-              }
+  //             }
         
-              .address h2 {
-                font-size: 20px;
-                margin-bottom: 10px;
-              }
+  //             .address h2 {
+  //               font-size: 20px;
+  //               margin-bottom: 10px;
+  //             }
         
-              .address p {
-                margin: 0;
-              }
+  //             .address p {
+  //               margin: 0;
+  //             }
         
-              .thanks {
-                font-size: 18px;
-                margin-top: 20px;
-                color: black;
+  //             .thanks {
+  //               font-size: 18px;
+  //               margin-top: 20px;
+  //               color: black;
 
-              }
+  //             }
         
-              .signature {
-                margin-top: 40px;
-                color: black;
+  //             .signature {
+  //               margin-top: 40px;
+  //               color: black;
 
-              }
+  //             }
         
-              .signature p {
-                margin: 0;
-              }
-            </style>
-          </head>
-          <body>
-            <h1>Order Confirmation</h1>
-            <p style="color:black;">Dear <b>${userData.firstName} ${userData.lastName}</b>,</p>
-            <p>Thank you for your recent purchase on our website. We have received your payment of <b>₹${totalAmount}</b> and have processed your order.</p>
-            <table>
-              <thead>
-                <tr>
-                  <th>Product Name</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                </tr>
-              </thead>
-              <tbody>
-                ${productInfo.map((product) => {
-          return `
-                            <tr>
-                              <td>${product.productId.name}</td>
-                              <td>${product.quantity}</td>
-                              <td>₹${product.productId.price}</td>
-                            </tr>
-                          `
-        }).join('')
-          }
-          <tr>
-          <td>Shipping Charge</td>
-          <td></td>
-          <td>₹100</td>
-        </tr>
-        <tr>
-          <td>Total</td>
-          <td></td>
-          <td>₹${totalAmount}</td>
-        </tr>
-              </tbody >
-            </table >
-            <div class="address">
-              <h2>Shipping Address</h2>
-              <p>${userData.firstName} ${userData.lastName}</p>
-              <p>${userData.address}</p>
-              <p>${userData.city}-${userData.zipCode}</p>
-              <p>${userData.userState}</p>
-            </div>
-            <p class="thanks">Thank you for choosing our website. If you have any questions or concerns, please don't hesitate to contact us.</p>
-            <div class="signature">
-              <p>Best regards,</p>
-              <p> <a href="https://e-shopit.vercel.app/" target="_blank">ShopIt.com</a></p>
-            </div>
-          </body >
-        </html >
-  `
+  //             .signature p {
+  //               margin: 0;
+  //             }
+  //           </style>
+  //         </head>
+  //         <body>
+  //           <h1>Order Confirmation</h1>
+  //           <p style="color:black;">Dear <b>${userData.firstName} ${userData.lastName}</b>,</p>
+  //           <p>Thank you for your recent purchase on our website. We have received your payment of <b>₹${totalAmount}</b> and have processed your order.</p>
+  //           <table>
+  //             <thead>
+  //               <tr>
+  //                 <th>Product Name</th>
+  //                 <th>Quantity</th>
+  //                 <th>Price</th>
+  //               </tr>
+  //             </thead>
+  //             <tbody>
+  //               ${productInfo.map((product) => {
+  //         return `
+  //                           <tr>
+  //                             <td>${product.productId.name}</td>
+  //                             <td>${product.quantity}</td>
+  //                             <td>₹${product.productId.price}</td>
+  //                           </tr>
+  //                         `
+  //       }).join('')
+  //         }
+  //         <tr>
+  //         <td>Shipping Charge</td>
+  //         <td></td>
+  //         <td>₹100</td>
+  //       </tr>
+  //       <tr>
+  //         <td>Total</td>
+  //         <td></td>
+  //         <td>₹${totalAmount}</td>
+  //       </tr>
+  //             </tbody >
+  //           </table >
+  //           <div class="address">
+  //             <h2>Shipping Address</h2>
+  //             <p>${userData.firstName} ${userData.lastName}</p>
+  //             <p>${userData.address}</p>
+  //             <p>${userData.city}-${userData.zipCode}</p>
+  //             <p>${userData.userState}</p>
+  //           </div>
+  //           <p class="thanks">Thank you for choosing our website. If you have any questions or concerns, please don't hesitate to contact us.</p>
+  //           <div class="signature">
+  //             <p>Best regards,</p>
+  //             <p> <a href="https://e-shopit.vercel.app/" target="_blank">ShopIt.com</a></p>
+  //           </div>
+  //         </body >
+  //       </html >
+  // `
 
-      }
+  //     }
 
-      transport.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-          // res.send({ msg: error });
-        }
-        else {
-          return res.send({ success, msg: "Order Confirm" })
-        }
-      })
+  //     transport.sendMail(mailOptions, (error, info) => {
+  //       if (error) {
+  //         console.log(error);
+  //         // res.send({ msg: error });
+  //       }
+  //       else {
+  //         return res.send({ success, msg: "Order Confirm" })
+  //       }
+  //     })
+
       await Payment.create({
         razorpay_order_id,
         razorpay_payment_id,
@@ -333,7 +334,8 @@ const paymentVerification = async (req, res) => {
       });
       const deleteCart = await Cart.deleteMany({ user: userInfo })
 
-      res.redirect(`${process.env.PAYMENT_SUCCESS}=${razorpay_payment_id} `);
+      res.redirect(`http://localhost:3000/cart`);
+      // res.redirect(`${process.env.PAYMENT_SUCCESS}=${razorpay_payment_id} `);
     }
     else {
       res.status(400).json({
